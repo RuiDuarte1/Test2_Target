56d5b4ee91e00b90a44303cbc9d3cf16269cda3a
Rui Duarte <ruycruzeiro9@gmail.com>
2023-03-08 12:15:10 -0300

    first commit

---
+++ 4.js
@@ -0,0 +1,17 @@
+// Equação do carro:
+// x1 = v1.t
+
+// Equação do caminhão:
+// x2 = 100 - v2.t
+
+// Cálculo do tempo de viagem:
+// t = 100/80  = 1,25h
+
+//Vel.Média  perdendo 0,17h + 1,25h = 1,42: 
+// v = 100/1,42 = 70,6
+
+//Cálculo do ponto de cruzamento:
+// x1 = x2 
+// x/v1 = (x-100)/-v2
+// x =  (v1.100)\v1+v2 = (110.100)\110+70,6
+// x = 60,9km
